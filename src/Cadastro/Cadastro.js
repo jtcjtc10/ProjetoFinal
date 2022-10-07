8ca864f0eb46a6d0023603430cb10fd061f094c8
Kleuber de Paula Jacob <00787744@araujo.corp>
2022-10-07 20:52:37 -0300

    Input Header and Footer component Cadastro

--- src/Cadastro/Cadastro.js
+++ src/Cadastro/Cadastro.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../Cadastro/Cad.css";
-import Login from "../Login/Login";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
+
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 export default function Cadastro() {
     const [tNome, setTxtNome] = React.useState("");
@@ -16,10 +18,10 @@ export default function Cadastro() {
 
     const MySwal = withReactContent(Swal);
 
-    const verificaCadastro = (e) => { 
+    const verificaCadastro = (e) => {
         e.preventDefault();
         console.log(tNome, tCpf, tRg, tEndereco, tEmail, tTelefone, tSenha, tConfirmSenha);
-        
+
         if (tNome === "" || tCpf === "" || tRg === "" || tEndereco === "" || tEmail === "" || tTelefone === "" || tSenha === "" || tConfirmSenha === "") {
             MySwal.fire({
                 title: 'Atenção!',
@@ -40,14 +42,16 @@ export default function Cadastro() {
                 allowEscapeKey: false,
                 allowOutsideClick: false
             }).then((result) => {
-                if(result.isConfirmed){
+                if (result.isConfirmed) {
                     window.location.href = "login.html";
                 }
             });
         }
-        
+
     }
     return (
+        <>
+            <Header/>
             <div className="container-fluid">
                 <div className="row justify-content-center align-items-center section-login">
                     <div className="col-lg-6 col-md-8 col-sm-10 modal-login text-center">
@@ -59,29 +63,29 @@ export default function Cadastro() {
                                     <div className="row mb-2 mt-3 justify-content-center">
                                         <label for="nome" className="col-sm-12 col-form-label label-login">Nome</label>
                                         <div className="col-sm-12">
-                                            <input type="text" className="form-control input-login" id="nome" 
-                                            onChange={(e) => setTxtNome(e.target.value)} value={tNome} placeholder="Nome"/>
+                                            <input type="text" className="form-control input-login" id="nome"
+                                                onChange={(e) => setTxtNome(e.target.value)} value={tNome} placeholder="Nome" />
                                         </div>
                                     </div>
                                     <div className="row mb-2 justify-content-center">
                                         <label for="cpf" className="col-sm-12 col-form-label label-login">CPF</label>
                                         <div className="col-sm-12">
                                             <input type="number" className="form-control input-login" id="cpf"
-                                            onChange={(e) => setTxtCpf(e.target.value)} value={tCpf} placeholder="CPF"/>
+                                                onChange={(e) => setTxtCpf(e.target.value)} value={tCpf} placeholder="CPF" />
                                         </div>
                                     </div>
                                     <div className="row mb-2 justify-content-center">
                                         <label for="rg" className="col-sm-12 col-form-label label-login">RG</label>
                                         <div className="col-sm-12">
                                             <input type="text" className="form-control input-login" id="rg"
-                                            onChange={(e) => setTxtRg(e.target.value)} value={tRg} placeholder="RG"/>
+                                                onChange={(e) => setTxtRg(e.target.value)} value={tRg} placeholder="RG" />
                                         </div>
                                     </div>
                                     <div className="row mb-2 justify-content-center">
                                         <label for="endereco" className="col-sm-12 col-form-label label-login">Endereço</label>
                                         <div className="col-sm-12">
                                             <input type="text" className="form-control input-login" id="endereco"
-                                                onChange={(e) => setTxtEndereco(e.target.value)} value={tEndereco} placeholder="Endereço"/>
+                                                onChange={(e) => setTxtEndereco(e.target.value)} value={tEndereco} placeholder="Endereço" />
                                         </div>
                                     </div>
                                 </div>
@@ -90,21 +94,21 @@ export default function Cadastro() {
                                         <label for="email" className="col-sm-12 col-form-label label-login">E-mail</label>
                                         <div className="col-sm-12">
                                             <input type="email" className="form-control input-login" id="email"
-                                               onChange={(e) => setTxtEmail(e.target.value)} value={tEmail} placeholder="E-mail"/>
+                                                onChange={(e) => setTxtEmail(e.target.value)} value={tEmail} placeholder="E-mail" />
                                         </div>
                                     </div>
                                     <div className="row mb-2 justify-content-center">
                                         <label for="telefone" className="col-sm-12 col-form-label label-login">Telefone</label>
                                         <div className="col-sm-12">
                                             <input type="number" className="form-control input-login" id="telefone"
-                                               onChange={(e) => setTxtTelefone(e.target.value)} value={tTelefone} placeholder="Telefone"/>
+                                                onChange={(e) => setTxtTelefone(e.target.value)} value={tTelefone} placeholder="Telefone" />
                                         </div>
                                     </div>
                                     <div className="row mb-2 justify-content-center">
                                         <label for="senha" className="col-sm-12 col-form-label label-login">Senha</label>
                                         <div className="col-sm-12">
                                             <input type="password" className="form-control input-login" id="senha"
-                                                onChange={(e) => setTxtSenha(e.target.value)} value={tSenha} placeholder="Senha"/>
+                                                onChange={(e) => setTxtSenha(e.target.value)} value={tSenha} placeholder="Senha" />
                                         </div>
                                     </div>
                                     <div className="row mb-2 justify-content-center">
@@ -112,7 +116,7 @@ export default function Cadastro() {
                                             Senha</label>
                                         <div className="col-sm-12">
                                             <input type="password" className="form-control input-login" id="confirmarSenha"
-                                                onChange={(e) => setTxtConfirmSenha(e.target.value)} value={tConfirmSenha} placeholder="Confirmar senha"/>
+                                                onChange={(e) => setTxtConfirmSenha(e.target.value)} value={tConfirmSenha} placeholder="Confirmar senha" />
                                         </div>
                                     </div>
                                 </div>
@@ -128,5 +132,7 @@ export default function Cadastro() {
                     </div>
                 </div>
             </div>
+            <Footer/>
+        </>
     )
 }
