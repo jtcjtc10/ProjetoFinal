b8746c080c19a6ef7adb59be0498f3914120e600
Joao Victor Ferreira Santos <00787736@araujo.corp>
2022-10-17 14:51:10 -0300

    Home

--- src/home/home.js
+++ src/home/home.js
@@ -32,15 +32,16 @@ export default function Home() {
                     <div id="jordanSection" className="row justify-content-center align-items-center card2 ">
                     <Cards2 />
                     </div>
-                    <div className=" main2">
+                    <div className=" row main2">
+                        
                         <div className=" main1-1">
-                        <img className="img1" src="/images/jordan1-crimson.png"  />
+                        <img className="img2" src="/images/jordan1-orange.png"  />
                         </div>
                         <div className=" main1-2">
                         <div className="text1">
                         <h2>Os Melhores Modelos!</h2>
-                        <h4>Cores Vivas estão no Nosso DNA</h4>
-                        <h4>Conheça nosso modelos</h4>
+                        <h4>Autencidade Garantida Pela Nike</h4>
+                        <h4>Compre Agora</h4>
                         </div>
                         </div>
                     </div>
